358205b0c37cfbf4aa82ee4b31843ebd01b68d24
Katsaros Lampros <katsaros.lampros01@gmail.com>
2026-03-15 23:12:57 +0200

    Add the dotenv in the server.js and update the app.get('/') task

--- server/server.js
+++ server/server.js
@@ -1,17 +1,26 @@
+require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const db=process.env.DB_URL;
 
 // Serve all static files (HTML, CSS, JS, images) from the client folder
 app.use(express.static(path.join(__dirname, '../client')));
 
-// Fallback: serve index.html for any unmatched route
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'));
-});
+app.use(express.json()); // Middleware to parse JSON bodies
+
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
+});
+
+app.get('/',function(req,res){
+    var options={
+        root: path.join(__dirname,'../client')
+    }
+    res.sendFile('index.html', options, function(err){
+        console.log(err);
+    })
 });
