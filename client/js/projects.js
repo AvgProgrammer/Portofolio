d3003f7b9c5e42c0469c4d1f561a5673c7a7f5ac
Katsaros Lampros <katsaros.lampros01@gmail.com>
2026-03-16 01:40:34 +0200

    Implementh the connection of the project.js with the server similar to index.js

--- client/js/projects.js
+++ client/js/projects.js
@@ -1,14 +1,42 @@
-var projectsTemp = {}
-
-let projectsScript = document.getElementById("Project-list-Template")
-
-projectsTemp.projectsList = Handlebars.compile(projectsScript.textContent)
-
-let projectsContent = projectsTemp.projectsList({projectsList: projects})
-
 window.addEventListener("DOMContentLoaded", main)
 
 function main(){
+    let projectsScript = document.getElementById("Project-list-Template")
+    window.myApp={}
+    myApp.projectsTemp= Handlebars.compile(projectsScript.textContent)
+    
+    fetchAndShowProjects()
+}
+
+function fetchAndShowProjects(){
+    let httpsheader=new Headers()
+    httpsheader.append("Accept","application/json")
+
+    let init={
+        method: "GET",
+        Headers: httpsheader
+    }
+
+    fetch("/api/projects", init)
+        .then(response => response.json())
+        .then(data => showProjects(data))
+        .catch(error => showError(error))
+}
+
+function showError(error){
+    console.error("Error fetching projects:", error);
+
+}
+
+function showProjects(data){
+    const projectsContent= data.map(project=>({
+        title: project.title,
+        description: project.description,
+    }))
+
+    const projectsTemplate= myApp.projectsTemp({
+        projectsList: projectsContent
+    })
     let projectList= document.querySelector("#projects")
-    projectList.innerHTML= projectsContent
+    projectList.innerHTML= projectsTemplate
 }
